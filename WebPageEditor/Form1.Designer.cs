b2702522708bfa53ff5467ef5fa692112dcae227
lexis <alex.layt.pro@gmail.com>
2017-08-16 18:25:30 +0300

    init commit; ready editor

---
+++ WebPageEditor/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WebPageEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbHtml = new System.Windows.Forms.TextBox();
+            this.tbCss = new System.Windows.Forms.TextBox();
+            this.previewBrowser = new System.Windows.Forms.WebBrowser();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.browserPanel = new System.Windows.Forms.Panel();
+            this.browserWidth = new System.Windows.Forms.TextBox();
+            this.xLabel = new System.Windows.Forms.Label();
+            this.browserHeight = new System.Windows.Forms.TextBox();
+            this.browserPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbHtml
+            // 
+            this.tbHtml.Location = new System.Drawing.Point(0, 0);
+            this.tbHtml.Multiline = true;
+            this.tbHtml.Name = "tbHtml";
+            this.tbHtml.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbHtml.Size = new System.Drawing.Size(493, 340);
+            this.tbHtml.TabIndex = 0;
+            this.tbHtml.TextChanged += new System.EventHandler(this.tbHtml_TextChanged);
+            this.tbHtml.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbEditor_KeyDown);
+            // 
+            // tbCss
+            // 
+            this.tbCss.Location = new System.Drawing.Point(0, 347);
+            this.tbCss.Multiline = true;
+            this.tbCss.Name = "tbCss";
+            this.tbCss.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbCss.Size = new System.Drawing.Size(493, 360);
+            this.tbCss.TabIndex = 1;
+            this.tbCss.TextChanged += new System.EventHandler(this.tbCss_TextChanged);
+            this.tbCss.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbEditor_KeyDown);
+            // 
+            // previewBrowser
+            // 
+            this.previewBrowser.Location = new System.Drawing.Point(3, 0);
+            this.previewBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.previewBrowser.Name = "previewBrowser";
+            this.previewBrowser.ScrollBarsEnabled = false;
+            this.previewBrowser.Size = new System.Drawing.Size(479, 675);
+            this.previewBrowser.TabIndex = 2;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(898, 684);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 28);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // browserPanel
+            // 
+            this.browserPanel.AutoScroll = true;
+            this.browserPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.browserPanel.Controls.Add(this.previewBrowser);
+            this.browserPanel.Location = new System.Drawing.Point(499, 0);
+            this.browserPanel.Name = "browserPanel";
+            this.browserPanel.Size = new System.Drawing.Size(485, 678);
+            this.browserPanel.TabIndex = 4;
+            // 
+            // browserWidth
+            // 
+            this.browserWidth.Location = new System.Drawing.Point(502, 685);
+            this.browserWidth.Name = "browserWidth";
+            this.browserWidth.Size = new System.Drawing.Size(100, 22);
+            this.browserWidth.TabIndex = 5;
+            this.browserWidth.Text = "1024";
+            this.browserWidth.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // xLabel
+            // 
+            this.xLabel.AutoSize = true;
+            this.xLabel.Location = new System.Drawing.Point(608, 688);
+            this.xLabel.Name = "xLabel";
+            this.xLabel.Size = new System.Drawing.Size(14, 17);
+            this.xLabel.TabIndex = 6;
+            this.xLabel.Text = "x";
+            // 
+            // browserHeight
+            // 
+            this.browserHeight.Location = new System.Drawing.Point(628, 685);
+            this.browserHeight.Name = "browserHeight";
+            this.browserHeight.Size = new System.Drawing.Size(100, 22);
+            this.browserHeight.TabIndex = 7;
+            this.browserHeight.Text = "800";
+            this.browserHeight.TextChanged += new System.EventHandler(this.browserHeight_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 719);
+            this.Controls.Add(this.browserHeight);
+            this.Controls.Add(this.xLabel);
+            this.Controls.Add(this.browserWidth);
+            this.Controls.Add(this.browserPanel);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.tbCss);
+            this.Controls.Add(this.tbHtml);
+            this.Name = "Form1";
+            this.Text = "WebDesign Editor";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.browserPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbHtml;
+        private System.Windows.Forms.TextBox tbCss;
+        private System.Windows.Forms.WebBrowser previewBrowser;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Panel browserPanel;
+        private System.Windows.Forms.TextBox browserWidth;
+        private System.Windows.Forms.Label xLabel;
+        private System.Windows.Forms.TextBox browserHeight;
+    }
+}
+
---
+++ WebPageEditor/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace WebPageEditor
+{
+    public partial class Form1 : Form
+    {
+        private String htmlText = "";
+        private String cssText = "";
+        private String htmlFormat = "<!DOCTYPE html><HTML><HEAD><STYLE>{1}</STYLE></HEAD><BODY>{0}</BODY></HTML>";
+        private Boolean viewUpdated = true;
+        private BackgroundWorker webViewUpdater;
+        private DoWorkEventHandler delegateUpdater;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.webViewUpdater = new BackgroundWorker();
+            this.delegateUpdater = new DoWorkEventHandler(backgroundHandler);
+            this.webViewUpdater.DoWork += this.delegateUpdater;
+            this.sizeControls();
+            this.initValues();
+        }
+
+        public void sizeControls()
+        {
+            this.tbHtml.Location = new Point{ X = 0, Y = 0};
+            this.tbHtml.Size = new Size { Width = (this.Width / 2 - 2), Height = (this.Height / 2 - 20) };
+
+            this.tbCss.Location = new Point { X = 0, Y = (this.tbHtml.Height + 4) };
+            this.tbCss.Size = new Size { Width = this.tbHtml.Width, Height = this.tbHtml.Height };
+
+            this.browserPanel.Location = new Point { X = this.tbHtml.Right + 2, Y = 0 };
+            this.browserPanel.Size = new Size { Width = (this.Width / 2 - 2), Height = (this.Height - this.btnRefresh.Height - 36) };
+
+            this.browserWidth.Location = new Point { X = this.tbCss.Right + 2, Y = (this.browserPanel.Bottom +  4) };
+            this.xLabel.Location = new Point { X = this.browserWidth.Right + 2, Y = this.browserWidth.Location.Y };
+            this.browserHeight.Location = new Point { X = this.xLabel.Right + 2, Y = this.browserWidth.Location.Y };
+
+            this.btnRefresh.Location = new Point { X = (this.browserPanel.Right - this.btnRefresh.Width - 7), Y = this.browserPanel.Bottom + 4};
+        }
+
+        public void initValues()
+        {
+            this.browserWidth.Text = this.previewBrowser.Width.ToString();
+            this.browserHeight.Text = this.previewBrowser.Height.ToString();
+
+            this.browserWidth.MouseWheel += new MouseEventHandler(browserSize_MouseWheel);
+            this.browserHeight.MouseWheel += new MouseEventHandler(browserSize_MouseWheel);
+
+            this.tbCss.MaxLength = Int32.MaxValue;
+            this.tbHtml.MaxLength = Int32.MaxValue;
+        }
+
+        void browserSize_MouseWheel(object sender, MouseEventArgs e)
+        {
+            Int32 value;
+            Boolean result = Int32.TryParse((sender as TextBox).Text, out value);
+            if (!result || value < 0)
+            {
+                (sender as TextBox).Text = "0";
+            }
+            else
+            {
+                (sender as TextBox).Text = e.Delta > 0 ? (value + 10).ToString() : (value - 10).ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.htmlText = this.tbHtml.Text;
+            this.cssText = this.tbCss.Text;
+            this.setCode(this.htmlText, this.cssText);
+            this.previewBrowser.Update();
+        }
+
+        protected void setCode(String html, String css = "")
+        {
+            this.previewBrowser.DocumentText = String.Format(this.htmlFormat, html, css);
+        }
+
+        protected void backgroundHandler(object sender, DoWorkEventArgs args)
+        {
+            do
+            {
+                Thread.Sleep(500);
+                this.previewBrowser.DocumentText = String.Format(this.htmlFormat, this.htmlText, this.cssText);
+                this.previewBrowser.Document.Window.ResizeTo(10, 10);
+                this.viewUpdated = true;
+            } while (!this.viewUpdated);
+        }
+
+        protected void refreshWebView()
+        {
+            this.viewUpdated = false;
+            if (!this.webViewUpdater.IsBusy)
+            {
+                this.webViewUpdater.RunWorkerAsync();
+            }
+        }
+
+        private void tbHtml_TextChanged(object sender, EventArgs e)
+        {
+            this.htmlText = (sender as TextBox).Text;
+            this.refreshWebView();
+        }
+
+        private void tbCss_TextChanged(object sender, EventArgs e)
+        {
+            this.cssText = (sender as TextBox).Text;
+            this.refreshWebView();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Int32 width;
+            Boolean result = Int32.TryParse((sender as TextBox).Text, out width);
+            if (!result)
+            {
+                (sender as TextBox).Text = this.previewBrowser.Width.ToString();
+            }
+            else
+            {
+                this.previewBrowser.Width = width;
+            }
+        }
+
+        private void browserHeight_TextChanged(object sender, EventArgs e)
+        {
+            Int32 height;
+            Boolean result = Int32.TryParse((sender as TextBox).Text, out height);
+            if (!result)
+            {
+                (sender as TextBox).Text = this.previewBrowser.Height.ToString();
+            }
+            else
+            {
+                this.previewBrowser.Height = height;
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.sizeControls();
+        }
+
+        private void tbEditor_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                (sender as TextBox).SelectAll();
+            }
+        }
+    }
+}
